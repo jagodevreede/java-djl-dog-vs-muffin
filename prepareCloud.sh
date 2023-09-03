edbfaf0883017e7d337f005cec8dc68cf3b74cde
Jago de Vreede <jagodevreede@gmail.com>
2023-09-03 19:16:16 +0200

    Made cloud username more configurable

--- prepareCloud.sh
+++ prepareCloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#!/bin/bash
+USERNAME=ubuntu
 if [ "$#" -ne 1 ]; then
   echo "Illegal number of parameters"
   echo "Start script: runOnCloud.sh ip_of_machine"
@@ -14,9 +14,9 @@ if [[ ! -f validation_set.tar.gz ]]; then
   tar -zcvf validation_set.tar.gz test-set
 fi
 
-ssh ubuntu@$1 'mkdir -p dog-vs-muffin'
-scp validation_set.tar.gz ubuntu@$1:./dog-vs-muffin/validation_set.tar.gz
-scp training_data.tar.gz ubuntu@$1:./dog-vs-muffin/training_data.tar.gz
+ssh $USERNAME@$1 'mkdir -p dog-vs-muffin'
+scp validation_set.tar.gz $USERNAME@$1:./dog-vs-muffin/validation_set.tar.gz
+scp training_data.tar.gz $USERNAME@$1:./dog-vs-muffin/training_data.tar.gz
 
-ssh ubuntu@$1 'tar -xf dog-vs-muffin/validation_set.tar.gz -C ./dog-vs-muffin/'
-ssh ubuntu@$1 'tar -xf dog-vs-muffin/training_data.tar.gz -C ./dog-vs-muffin/'
+ssh $USERNAME@$1 'tar -xf dog-vs-muffin/validation_set.tar.gz -C ./dog-vs-muffin/'
+ssh $USERNAME@$1 'tar -xf dog-vs-muffin/training_data.tar.gz -C ./dog-vs-muffin/'
--- runOnCloud.sh
+++ runOnCloud.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+USERNAME=ubuntu
 if [ "$#" -ne 1 ]; then
   echo "Illegal number of parameters"
   echo "Start script: runOnCloud.sh ip_of_machine"
   exit
 fi
 
-
 mvn clean package -P gpu
 
-scp djl/target/java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar ubuntu@$1:./dog-vs-muffin/java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar
+scp djl/target/java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar $USERNAME@$1:./dog-vs-muffin/java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar
 
 # kill old process if there are any
-ssh ubuntu@$1 'killall -9 java; rm -rf dog-vs-muffin/models'
+ssh $USERNAME@$1 'killall -9 java; rm -rf dog-vs-muffin/models'
 
 # node manager and gpu node exporter are running on port 9100 and 9835 on the remote machine, map them to local host so that we can always scrape the same ip (localhost)
-ssh -L localhost:9101:localhost:9100 -L localhost:9835:localhost:9835 ubuntu@$1 'cd dog-vs-muffin; /home/ubuntu/.sdkman/candidates/java/17.0.3.6.1-amzn/bin/java -jar java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar learn'
+ssh -L localhost:9101:localhost:9100 -L localhost:9835:localhost:9835 $USERNAME@$1 'cd dog-vs-muffin; /home/$USERNAME/.sdkman/candidates/java/17.0.3.6.1-amzn/bin/java -jar java-djl-dog-vs-muffin-djl-1.0-SNAPSHOT-shaded.jar learn'
